536dc5e3a0f5d501a95284c16c8d472e211197c8
David Rodenas <david.rodenas+gt@gmail.com>
2022-08-03 21:37:59 +0200

    backend: the frontendSimulator improves error messages

--- src/test/java/com/drpicox/game/fixtures/FrontendSimulator.java
+++ src/test/java/com/drpicox/game/fixtures/FrontendSimulator.java
@@ -1,14 +1,16 @@
 package com.drpicox.game.fixtures;
 
-import com.drpicox.game.game.api.GameResponse;
 import com.google.gson.Gson;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -34,48 +36,82 @@ public class FrontendSimulator {
     }
 
     public <T> T get(String url, Class<T> type) {
-        String result = null;
-        try {
-            result = mockMvc.perform(MockMvcRequestBuilders.get(url))
-                    .andExpect(status().isOk())
-                    .andReturn()
-                    .getResponse()
-                    .getContentAsString();
-        } catch (Exception e) {
-            throw new RuntimeException("Error while executing the API REST Call: GET " + url, e);
-        }
+        var request = MockMvcRequestBuilders.get(url);
+        var result = performApiCall(url, request, type);
+        interactions.add(new FrontendBackendInteraction("GET", url, null, result));
 
-        var list = gson.fromJson(result, type);
-
-        interactions.add(new FrontendBackendInteraction("GET", url, null, list));
-
-        return list;
+        return result;
     }
 
-
-
     public <T> T post(String url, Object requestBody, Class<T> type) {
-        String result = null;
         var requestBodyJson = gson.toJson(requestBody);
+        var request = MockMvcRequestBuilders.post(url)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(requestBodyJson)
+            .characterEncoding("utf-8");
+
+        var result = performApiCall(url, request, type);
+        interactions.add(new FrontendBackendInteraction("POST", url, requestBody, result));
+
+        return result;
+    }
+
+    private <T> T performApiCall(String url, MockHttpServletRequestBuilder requestBuilder, Class<T> type) {
+        String resultJson = null;
+        MvcResult result = null;
         try {
-            result = mockMvc.perform(MockMvcRequestBuilders.post(url)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(requestBodyJson)
-                    .characterEncoding("utf-8")
-                )
-                .andExpect(status().isOk())
-                .andReturn()
-                .getResponse()
-                .getContentAsString();
+            result = mockMvc.perform(requestBuilder).andReturn();
+            resultJson = result.getResponse().getContentAsString();
+
+            var exception = result.getResolvedException();
+            if (exception != null) throw exception;
+
+            if (result.getResponse().getStatus() != 200)
+                throw new RuntimeException("The api call did not returned status 200.");
         } catch (Exception e) {
-            throw new RuntimeException("Error while executing the API REST Call: POST " + url, e);
+            if (result == null)
+                throw new RuntimeException("Error while executing the API REST Call: POST " + url, e);
+
+            var message = new StringBuilder();
+            var request = result.getRequest();
+            var response = result.getResponse();
+
+            message.append("The api call ")
+                .append(request.getMethod()).append(" ").append(url)
+                .append(" has failed.");
+
+            var status = response.getStatus();
+            if (status == 404) message
+                .append("\nIt looks like that there is no @RestController ")
+                .append("for the api call ")
+                .append(request.getMethod()).append(" ").append(url)
+                .append(". Make sure that you have an api controller with @RestController ")
+                .append("with the corresponding annotations of @RequestMapping, @GetMapping, @PostMapping ")
+                .append("or verify the URL is correct.");
+
+            if (status == 500) message
+                .append("\nIt looks like the @RestController ")
+                .append("" + result.getHandler())
+                .append("has failed to fulfill the request. ")
+                .append("Please, check the stacktrace.");
+
+            try {
+                message
+                    .append("\n- method   : ").append(request.getMethod())
+                    .append("\n- url      : ").append(url)
+                    .append("\n- body     : ").append(request.getContentAsString())
+                    .append("\n- status   : ").append(response.getStatus())
+                    .append("\n- handler  : ").append(result.getHandler())
+                    .append("\n- exception: ").append(result.getResolvedException());
+            } catch (UnsupportedEncodingException ex) {
+                throw new RuntimeException(ex);
+            }
+
+            throw new RuntimeException(message.toString(), e);
         }
 
-        var list = gson.fromJson(result, type);
-
-        interactions.add(new FrontendBackendInteraction("POST", url, requestBody, list));
-
-        return list;
+        var resultObject = gson.fromJson(resultJson, type);
+        return resultObject;
     }
 
     public void clear(String postId) {
