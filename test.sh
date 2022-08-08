2c21f74d6b88734c388e66d99da60f45ba660b4e
David Rodenas <david.rodenas+gt@gmail.com>
2022-08-08 14:40:34 +0200

    test.sh: remove old tests so there is no unwanted stuff present

--- test.sh
+++ test.sh
@@ -5,6 +5,7 @@ error() {
   exit 1;
 }
 
+rm -fr $(find src -name 'Post_*_Test.*')
 CI=1 yarn create-tests || error "yarn create-tests"
 ./mvnw test            || error "./mvwn test"
 CI=1 yarn test         || error "yarn test"
